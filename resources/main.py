79fd76ffece9159a243afa93f2f752d046084edb
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-04-14 10:44:23 -0300

    plot to show f(n)

--- resources/main.py
+++ resources/main.py
@@ -8,14 +8,14 @@ def main():
     while (number > 1):
         number = step(number)
         numbers.append(number)
-
+    print(number)
     plt.plot(numbers)
     plt.show()
 
 
 def step(number):
     if number % 2 == 0:
-        number>>=2
+        number>>=1
     else:
         number = (3 * number) + 1
     return number
